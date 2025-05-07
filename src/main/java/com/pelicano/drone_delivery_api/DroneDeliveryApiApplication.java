775dfec4c47783fe849719266f66ae11b71a4e03
Janice Pelicano <qjvpelicano@gmail.com>
2025-05-07 08:09:03 +0800

    Initial commit

---
+++ src/main/java/com/pelicano/drone_delivery_api/DroneDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.pelicano.drone_delivery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pelicano/drone_delivery_api/DroneDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelicano.drone_delivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
